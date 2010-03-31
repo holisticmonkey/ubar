ee3515e5862f4d7a337b7672719518d31dd00054
joshganderson <joshganderson@b226c250-3293-11de-9c63-4752e237b823>
2010-03-31 07:31:37 +0000

    Changing version to 0.1.00 until can have independent evaluation as to completeness of framework. Adding running of phpunit tests to build. Completing first round of phpdoc like comments. Adding a stub for README. This should be a very simple quickstart and have links to further info.

--- core/Dispatcher.php
+++ core/Dispatcher.php
@@ -1,23 +1,109 @@
 <?php
+/**
+ * Class definition for Action
+ * @package		core
+ */
+
+/**
+ * Dispatcher for all requests
+ *
+ * This class, coupled with an Action, does most of the work in this
+ * framework. It parses the requested action out of the URL, instantiates the
+ * action, populates user input, executes the action and performes the
+ * appropriate response.
+ *
+ * @author		Joshua A. Ganderson <jag@josh.com>
+ * @link		http://www.holisticmonkey.com/Framework.action
+ * @copyright	Copyright (c) 2010, Joshua A. Ganderson
+ * @license		http://www.gnu.org/licenses/gpl.html GNU General Public License v3
+ * @package		core
+ *
+ * @todo Allow override of action extension, currently fixed at ".action"
+ */
 class Dispatcher {
 
-	// TODO: allow override of action identifier
+	/**
+	 * Regular expression to parse the action name out of the request URL.
+	 *
+	 * EXAMPLE: match "Foo" in /Foo.action in order to load the "Foo"
+	 * action definition.
+	 *
+	 * OPTIONS: utf-8
+	 */
 	const ACTION_REGEX = '/([^\/]+)\.action/u';
 
-	// multiline, extra analysis, utf-8, ungreedy
-	// get anything like ${foo} and match the 'foo' part for action method call
+	/**
+	 * Regular expression to parse expression calls out of result strings.
+	 * This is typically used to direct the user to a page that requires a GET
+	 * param, such as postID, to be displayed correctly.
+	 *
+	 * EXAMPLE: match "referringPage" and substitute with value from
+	 * getReferringPage() in action when evaluating the result,
+	 * <result name="USER_ERROR" type="url">Login.action?ref=${refPage}</result>
+	 *
+	 * NOTE: Right now this requires using the url result type. In future
+	 * iterations, results will be able to take params and auto-populate GET
+	 * params.
+	 *
+	 * OPTIONS: multiline, extra analysis, utf-8, ungreedy
+	 */
 	const RESULT_EXPRESSION_REGEX = '/\${([^$]*)}/mSuU';
 
+	/**
+	 * Instance of a convenience class that provides a action name to action
+	 * lookup.
+	 *
+	 * @see ActionMapper
+	 */
 	private $actionMapper;
 
+	/**
+	 * Current action instance being executed. This will extend Action and is
+	 * found by parsing the action name from the requested URL and looking up
+	 * the action class associated with that name.
+	 *
+	 * @see Action
+	 */
 	private $action;
 
+	/**
+	 * Definition for the current action instance being executed. This contains
+	 * information about the action class, asociated views, templates, etc.
+	 * The definition is created from ubar.xml and is found using the
+	 * ActionMapper.
+	 *
+	 * @see ActionMapper
+	 * @see $actionMapper
+	 */
 	private $actionDef;
 
+	/**
+	 * Construct the dispatcher. This just sets up the dispatcher for
+	 * definition retrieval.
+	 *
+	 * @see Dispatcher::$actionMapper
+	 * @see ActionMapper
+	 */
 	public function __construct($configPath) {
 		$this->actionMapper = new ActionMapper($configPath);
 	}
 
+	/**
+	 * Dispatch request to appropriate result. Typical usage is to retrieve the
+	 * action name from the requested URL, however, this may be overriden for
+	 * testing purposes.
+	 *
+	 * Dispatching renders the appropriate view, file, or forwards to another
+	 * action or url. Further result types may be supported in the future.
+	 * Behaviors associated with a given action name are defined in ubar.xml.
+	 *
+	 * NOTE: This is a public method so that it may be called from the init
+	 * script or tests. It is not recommended that you call it directly
+	 * elsewhere.
+	 *
+	 * @param string $actionString Overriding action string. Use not
+	 * recommended.
+	 */
 	public function dispatch($actionString = null) {
 		// clear transient values in case part of an action forward
 		$this->action = null;
@@ -142,6 +228,17 @@ class Dispatcher {
 		}
 	}
 
+	/**
+	 * Initialize view path if defined. This may be set in the result or
+	 * in the action definition.
+	 *
+	 * @throws ViewNotFoundException if the view is defined but the file does
+	 * not exist.
+	 * @throws Exception if no view was defined. This method is only called
+	 * when a view MUST exist.
+	 *
+	 * @see Dispatcher::renderPage()
+	 */
 	private function initView(Result $result) {
 		$viewPath = $result->getViewLocation();
 		if(is_null($viewPath)) {
@@ -159,6 +256,18 @@ class Dispatcher {
 		}
 	}
 
+	/**
+	 * Render a page view. This either renders a view file within an action
+	 * context or renders a view within it's template. This method is only
+	 * called if the result type is a page.
+	 *
+	 * @param class $result Result definition to get view information from.
+	 *
+	 * @throws Exception if a template is referenced but no definition was
+	 * found or the definition was found but the file was not.
+	 *
+	 * @see Dispatcher::dispatch()
+	 */
 	public function renderPage(Result $result) {
 		// set the view or throw error if not defined
 		$this->initView($result);
@@ -198,11 +307,22 @@ class Dispatcher {
 	}
 
 	// render the view associated with the action
+	/**
+	 * Render the page body. This is called if the result type is page and
+	 * no template was used.
+	 *
+	 * @see Dispatcher::renderPage()
+	 */
 	public function renderBody() {
 		require_once ($this->action->getView());
 	}
 
 	// try to set each get and post param in the action
+	/**
+	 * Attempt to populate any GET or POST params in the action.
+	 *
+	 * @see Action::set()
+	 */
 	private function populateUserInput() {
 		foreach ($_GET as $key => $val) {
 			$this->action->set($key, $val);
@@ -212,19 +332,30 @@ class Dispatcher {
 		}
 	}
 
-	private function evaluateResultString($result) {
+	/**
+	 * Perform expression evaluation on the result target. This is currently
+	 * only used for url type results. It may be used to insert GET params into
+	 * the URL. For instance, your intended location after a submit action is
+	 * Blog.action?blogId=34. In that case, you would use
+	 * Blog.action?blogId=${blogId} where your action has a public method,
+	 * getBlogId().
+	 *
+	 * @see Dispatcher::RESULT_EXPRESSION_REGEX
+	 * @see Dispatcher::dispatcher()
+	 */
+	private function evaluateResultString($target) {
 		// find all instances of ${XXXXX} and replace with getXXXXX
 		// while you find something that looks like a directive, try to process it
-			while(preg_match(self::RESULT_EXPRESSION_REGEX, $result, $match)) {
-				// get the key which should match a public method in the action
-				$key = trim($match[1]);
+		while(preg_match(self::RESULT_EXPRESSION_REGEX, $target, $match)) {
+			// get the key which should match a public method in the action
+			$key = trim($match[1]);
 
-				$methodName = $this->action->findMethodName($key);
-				$value = $this->action->$methodName();
-				$result = str_replace($match[0], $value, $result);
-			}
+			$methodName = $this->action->findMethodName($key);
+			$value = $this->action->$methodName();
+			$target = str_replace($match[0], $value, $target);
+		}
 
-		return $result;
+		return $target;
 	}
 }
 ?>
